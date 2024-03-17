92bcfc57162bbbe23604585434452ccf484d43be
Selma Aneer <selmalavi@gmail.comm>
2024-03-18 00:32:33 +0530

    final code
    mern app deployment

---
+++ wellness369backend/index.js
@@ -0,0 +1,45 @@
+zconst express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const bodyParser = require('body-parser');
+const authRoute = require("./Routes/AuthRoute");
+const profileRoute = require("./Routes/ProfilePage")
+const workoutRoute = require("./Routes/WorkoutPage")
+const path = require('path')
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+const dbURL = process.env.DB_URL;
+
+app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:3001', 
+  credentials: true 
+}));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+//....for deploy
+
+app.use(express.static(path.join(__dirname, './welness360/build')));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./wellness360/build/index.html"));
+});
+
+
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
+
+app.use("/", authRoute);
+app.use("/profile", profileRoute);
+app.use("/users",workoutRoute);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
